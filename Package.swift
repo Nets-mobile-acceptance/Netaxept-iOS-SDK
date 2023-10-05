6f16139ec7dc639fa005603000102502bd1c4d98
Aleksei Zaikin <aleksi.zaikin@gmail.com>
2023-10-05 19:55:22 +0300

    Update checksum

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
       .binaryTarget(
          name: "Pia",
          url: "https://github.com/Nets-mobile-acceptance/Netaxept-iOS-SDK/raw/master/Pia.xcframework.zip",
-         checksum: "cb494da865088028f7a48333f09e1e2c6d19a472521d50efdc271cbe58a6a199"
+         checksum: "e4e3f99400b80d9e646dc44ef9058968d9ac88571db1dd8a16d1ad34f149fd96"
       )
    ]
 )
