a440f36d348b644bd0b806e2c5233caddafbc69e
Mateusz Figlewicz <mateusz.figlewicz@external.nexigroup.com>
2024-02-22 12:44:00 +0100

    Update Pia.xcframework.zip that containts correct Privacy Manifest

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
       .binaryTarget(
          name: "Pia",
          url: "https://github.com/Nets-mobile-acceptance/Netaxept-iOS-SDK/raw/master/Pia.xcframework.zip",
-         checksum: "82638b6c966beff8d68fcfecac173c471ed50b16607e97bbc94b6f72e9f3d3ef"
+         checksum: "06b15383b28b7f4a369522093a1d9dc222ec8ebf685da6f7eed511f019a73544"
       )
    ]
 )
