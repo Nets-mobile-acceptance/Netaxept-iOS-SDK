06e31f059054cde05822fa004355534facf0d40d
Mateusz Figlewicz <mateusz.figlewicz@external.nexigroup.com>
2026-04-16 15:59:05 +0200

    Update Package.swift
    Update Package manifest

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
    targets: [
       .binaryTarget(
          name: "Pia",
-         url: "https://github.com/Nets-mobile-acceptance/Netaxept-iOS-SDK/raw/master/Pia.xcframework.zip",
+         url: "https://github.com/Nets-mobile-acceptance/Netaxept-iOS-SDK/releases/download/2.7.4/Pia.xcframework.zip",
          checksum: "f4ed742a79f2c8858ff1252057717f0aff5cc18353dd10574c6cb1dececca20c"
       )
    ]
